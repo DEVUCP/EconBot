78bcb4dd29fdaa408835211c3c93cf79403cc639
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2024-07-24 13:56:58 +0300

    Test print

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
-    print("ay 7aga")
+    print("Test")
 
 client.run(os.getenv("econtoken"))
