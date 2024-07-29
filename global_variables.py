ef9b52dc2f0126da31708e3c54cb8dec71b368a1
OH221105 <OMHN05@protonmail.com>
2024-07-29 19:36:17 +0300

    Removed 'global_variables.py' remnant.

--- global_variables.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-
-# Discord Essential variables
-intents =  discord.Intents.all()
-client = discord.Client(intents=intents)
-
-
-user_dict = {} # 1067541126518677664: serverid, list[econessentials.User]
