a87e71edfb6309b6ef2aeb0565e868640117cd3e
OH221105 <OMHN05@protonmail.com>
2024-08-14 03:58:13 +0300

    Created 'DisplayEnergy()' commands display function and setup 'DisplayClock' embed with dynamic colors.

--- commands/display.py
+++ commands/display.py
@@ -15,10 +15,42 @@ async def DisplayClock(message : discord.Message) -> None:
     time = utils.GetClockTime(initial_time=singletons.start_time)
     
     formatted_time = time["clock"].strftime("%I:%M %p")
-    week_day = time["day"]
+    week_day = time["week day"]
 
-    await message.reply(f"{week_day}, {formatted_time}")
+    color = discord.Color.dark_purple()
 
+    if time["clock"].hour <= 5 or time["clock"].hour >= 21:
+       # print("night")
+        color = 0x0c072e # Dark Blurple
+
+    elif time["clock"].hour > 5 and time["clock"].hour < 12:
+       # print("morning")
+        color = 0x65e3fe # light blue
+
+    elif time["clock"].hour >= 12 and time["clock"].hour < 15:
+       # print("afternoon")
+        color = 0xFFD0AA # light yellow
+
+    elif time["clock"].hour >= 17 and time["clock"].hour < 19:
+       # print("early evening")
+        color = 0xFD997F # light red
+
+    elif time["clock"].hour >= 19 and time["clock"].hour < 21:
+       # print("late evening")
+        color = 0x526079 # light purple
+
+    embed = discord.Embed(title=f"{week_day}, {formatted_time}",color=color)
+    embed.set_footer(text=f"Day {time['days']}")
+
+    await message.reply(embed=embed)
+
+async def DisplayEnergy(message : discord.Message) -> None:
+    """Displays user's energy bar."""
+    # await message.reply("Display Energy Invoked!") # Uncomment when debugging.
+    
+    user = utils.FindUser(uid=message.author.id, sid=message.guild.id)
+    embed = discord.Embed(title=user.energy.GetEnergyBar())
+    await message.reply(embed=embed)
 
 
 async def DisplayMarket(message : discord.Message, command : list[str]) -> None:
