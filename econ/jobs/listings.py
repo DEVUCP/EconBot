b80345c4b4e4787a23104c3d13b0ee2774ca2f1f
DEVUCP <OMHN05@protonmail.com>
2024-09-01 19:12:54 +0300

    Created 'listings.py' module.

---
+++ econ/jobs/listings.py
@@ -0,0 +1,48 @@
+import random
+from econ.jobs import jobs, job
+import constants
+import datetime
+from utils import GetTimeDelta
+
+
+listing = []
+last_updated = datetime.datetime.now()
+
+def GenerateListings() -> None:
+    """Generates a new listing."""
+    global listing
+    new_listing = []
+    for i in range(constants.LISTING_LEN):
+
+        new_job = random.choice(list(jobs.jobs.values())) # Generate a random job
+
+        if new_job in new_listing: # If the job is already in the listing, generate a new one
+            i-=1
+            continue
+
+        new_listing.append(new_job)
+
+    listing = new_listing # Update the listing
+
+def CheckUpdate() -> bool:
+    """Checks if the listing needs to be updated."""
+    global listing, last_updated
+    time_since_update = GetTimeDelta(initial_time=last_updated) # Time since last listing update.
+
+    if time_since_update["days"] > constants.LISTING_UPDATE_TIME:
+        GenerateListings()
+        last_updated = datetime.datetime.now()
+        return True
+    
+    return False
+
+def GetListing() -> list[job.Job]:
+    global listing
+    """Returns the listing."""
+    CheckUpdate()
+    return listing
+
+def GetLastUpdated() -> datetime.datetime:
+    """Returns the last updated time."""
+    global last_updated
+    return last_updated
