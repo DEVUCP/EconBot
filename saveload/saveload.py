8a928371453932bdf2edb8f16a5adb683f7607ba
DEVUCP <OMHN05@protonmail.com>
2024-09-21 22:17:21 +0300

    Implemented 'GetFilePath()' and 'print_colored()' 'in saveload.py'.

--- saveload/saveload.py
+++ saveload/saveload.py
@@ -6,15 +6,13 @@ import pickle
 import threading
 import time
 import asyncio
+from utils import GetFilePath
 
 loaded = False
 save_task : asyncio.Task
 
-# Get the directory where the script is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Construct the full path to the save file
-save_path = os.path.join(script_dir, 'userdata.pkl')
+save_path = GetFilePath(filename='userdata.pkl')
 
 print(save_path)
 
@@ -28,7 +26,7 @@ async def timed_save(interval):
 
 async def initiate_save():
     global save_task
-    print("Initiating timed save..")
+    
     save_task = asyncio.create_task(timed_save(constants.SAVE_INTERVAL))
 
 
@@ -49,7 +47,7 @@ async def SaveUserDict() -> None:
     try:
         with open(save_path, 'wb') as file:
             pickle.dump(singletons.user_dict, file=file)
-            print("--- Saved Userdata Successfully ---")
+            singletons.print_colored(". . . Saved Userdata Successfully !", "green")
     except Exception as err:
         print(f"Error: {err}")
 
@@ -62,7 +60,7 @@ def LoadUserDict() -> bool:
 
 def LoadMarketPages() -> bool:
     """Loads market item list into market pages."""
-    print(f"Loading market ...")
+    singletons.print_colored(f"[ LOADING MARKET . . . ]", "yellow")
     pages = math.ceil(len(singletons.market) / constants.PAGE_LEN)
     for i in range(pages):
         for j in range(constants.PAGE_LEN):
@@ -77,11 +75,12 @@ def LoadMarketPages() -> bool:
     else:
         if singletons.market_pages[-1] == []:
             singletons.market_pages[-1].remove()
+        singletons.print_colored(f"[ SUCCESSFULLY LOADED MARKET ! ]", "green")
         return True
     
 def LoadBlackMarketPages() -> bool:
     """Loads blackmarket item list into blackmarket pages."""
-    print(f"Loading blackmarket ...")
+    singletons.print_colored(f"[ LOADING BLACKMARKET . . . ] ", "yellow")
     pages = math.ceil(len(singletons.black_market) / constants.PAGE_LEN)
     for i in range(pages):
         for j in range(constants.PAGE_LEN):
@@ -96,76 +95,5 @@ def LoadBlackMarketPages() -> bool:
     else:
         if singletons.black_market_pages[-1] == []:
             singletons.black_market_pages[-1].remove()
+        singletons.print_colored(f"[ SUCCESSFULLY LOADED BLACKMARKET ! ]", "green")
         return True
-    
-
-# def SaveUserDict() -> bool:
-#     try:
-#         data = {}
-#         for server_id, user_list in singletons.user_dict.items():
-#             data[str(server_id)] = []
-#             for user in user_list:
-#                 user_data = {
-#                     "uid": user.uid,
-#                     "bank_acc": {
-#                         "cash_on_hand": user.bank_acc.GetCashOnHand(),
-#                         "deposit": user.bank_acc.GetDeposit()
-#                     },
-#                     "inventory": []
-#                 }
-#                 for page in user.inventory:
-#                     page_data = []
-#                     for item in page:
-#                         item_data = {
-#                             "name": item.GetName(),
-#                             "quantity": item.GetQuantity(),
-#                             "cost": item.GetCost()
-#                         }
-#                         page_data.append(item_data)
-#                     user_data["inventory"].append(page_data)
-#                 data[str(server_id)].append(user_data)
-        
-#         with open("user_data.json", "w") as f:
-#             json.dump(data, f, indent=4)
-#         return True
-#     except Exception as e:
-#         print(f"Error saving user data: {e}")
-#         return False
-
-
-# def LoadUserDict() -> bool:
-#     try:
-#         if not os.path.exists("user_data.json"):
-#             return False
-
-#         with open("user_data.json", "r") as f:
-#             data = json.load(f)
-
-#         singletons.user_dict.clear()
-#         for server_id, user_list in data.items():
-#             singletons.user_dict[int(server_id)] = []
-#             for user_data in user_list:
-#                 user = econessentials.User(uid=user_data["uid"])
-#                 user.bank_acc.SetCashOnHand(user_data["bank_acc"]["cash_on_hand"])
-#                 user.bank_acc.SetDeposit(user_data["bank_acc"]["deposit"])
-                
-#                 user.inventory = []
-#                 for page in user_data["inventory"]:
-#                     inventory_page = []
-#                     for item_data in page:
-#                         if item_data["name"] == "Insult Bag":
-#                             item = items.InsultBag(quantity=item_data["quantity"])
-#                         elif item_data["name"] == "Complement Bag":
-#                             item = items.ComplementBag(quantity=item_data["quantity"])
-#                         else:
-#                             continue
-#                         item.SetCost(item_data["cost"])
-#                         inventory_page.append(item)
-#                     user.inventory.append(inventory_page)
-                
-#                 singletons.user_dict[int(server_id)].append(user)
-        
-#         return True
-#     except Exception as e:
-#         print(f"Error loading user data: {e}")
-#         return False
